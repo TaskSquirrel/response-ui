7612e4b03caabc9d1bf4b0e4d8f07784d26ffc6a
Benjamin Huang <ben.huang06@gmail.com>
2019-04-23 14:54:16 -0400

    Upload form module

---
+++ src/components/Form/Upload.js
@@ -0,0 +1,84 @@
+import React, { useState } from "react";
+import classNames from "classnames";
+import PropTypes from "prop-types";
+
+import noop from "../../utils/noop";
+
+import styles from "./Upload.module.scss";
+
+const Upload = ({
+    file,
+    onUpload
+}) => {
+    const [hovering, setHovering] = useState(false);
+
+    function renderFileDialog() {
+        if (hovering) {
+            return "Drop the file!";
+        }
+
+        if (!file) {
+            return (
+                <React.Fragment>
+                    <span
+                        className={ styles.choose }
+                    >
+                        Choose
+                    </span>
+                    <span>
+                        { " or drag files here" }
+                    </span>
+                </React.Fragment>
+            );
+        }
+
+        return file.name;
+    }
+
+    function uploadFiles(files) {
+        onUpload(files.item(0));
+    }
+
+    function handleFileUpload({ target: { files } }) {
+        uploadFiles(files);
+    }
+
+    function handleDrop(e) {
+        e.preventDefault();
+        setHovering(false);
+
+        uploadFiles(e.dataTransfer.files);
+    }
+
+    return (
+        <div
+            className={ styles.upload }
+        >
+            <input
+                type="file"
+                className={ classNames(
+                    styles.file
+                ) }
+                onChange={ handleFileUpload }
+                onDragOver={ () => setHovering(true) }
+                onDragLeave={ () => setHovering(false) }
+                onDrop={ handleDrop }
+            />
+            <div>
+                { renderFileDialog() }
+            </div>
+        </div>
+    );
+};
+
+Upload.propTypes = {
+    file: PropTypes.instanceOf(File),
+    onUpload: PropTypes.func
+};
+
+Upload.defaultProps = {
+    file: null,
+    onUpload: noop
+};
+
+export default Upload;
