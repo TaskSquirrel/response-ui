43902fd24e224541ae510bfafd176b9364eff39c
Benjamin Huang <ben.huang06@gmail.com>
2019-05-04 23:48:19 -0400

    Start of `ActionCard`

---
+++ src/components/UI/ActionCard.js
@@ -0,0 +1,70 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import classNames from "classnames";
+import PropTypes from "prop-types";
+
+import CardBase from "./CardBase";
+
+import styles from "./ActionCard.module.scss";
+
+const ActionCard = ({
+    className,
+    title,
+    subtitle,
+    link
+}) => {
+    function renderCard() {
+        return (
+            <CardBase
+                className={ classNames(
+                    styles.card,
+                    className
+                ) }
+            >
+                <div
+                    className={ styles.inner }
+                >
+                    <div
+                        className={ styles.title }
+                    >
+                        { title }
+                    </div>
+                    {
+                        subtitle && (
+                            <div
+                                className={ styles.subtitle }
+                            >
+                                { subtitle }
+                            </div>
+                        )
+                    }
+                </div>
+            </CardBase>
+        );
+    }
+
+    return link
+        ? (
+            <Link
+                to={ link }
+            >
+                { renderCard() }
+            </Link>
+        )
+        : renderCard();
+};
+
+ActionCard.propTypes = {
+    className: PropTypes.string,
+    title: PropTypes.string.isRequired,
+    subtitle: PropTypes.string,
+    link: PropTypes.string
+};
+
+ActionCard.defaultProps = {
+    className: null,
+    subtitle: "",
+    link: null
+};
+
+export default ActionCard;
